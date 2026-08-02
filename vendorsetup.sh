984a9f10786c4c6efb1efa20acca79467fbb6506
一スAciss <orlannihboss@gmail.com>
2026-08-03 00:27:09 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,5 +1,4 @@
 # common
-
 git clone https://github.com/orlan-kecap/device_xiaomi_sm6150-common.git -b 16.2 device/xiaomi/sm6150-common
 git clone https://github.com/orlan-kecap/vendor_xiaomi_sm6150-common.git -b 16.2 vendor/xiaomi/sm6150-common
 
@@ -12,6 +11,10 @@ git clone https://github.com/orlan-kecap/Aciss-mainline.git -b los kernel/xiaomi
 # hardware 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
 
-# Miyi
-git clone https://github.com/manipvlator/device_xiaomi_miuicamera-sweet.git device/xiaomi/miuicamera-sweet
-git clone https://github.com/manipvlator/vendor_xiaomi_miuicamera-sweet.git -b 16 vendor/xiaomi/miuicamera-sweet
+# Miui Camera
+git clone https://github.com/xiaomi-sm6150/android_device_xiaomi_miuicamera-sweet.git device/xiaomi/miuicamera-sweet
+git clone https://github.com/xiaomi-sm6150/proprietary_vendor_miuicamera-sweet.git vendor/xiaomi/miuicamera-sweet
+
+# DolbyAtmos
+git clone https://github.com/manipvlator/proprietary_vendor_sony_dolby.git vendor/sony/dolby
+git clone https://github.com/manipvlator/android_packages_apps_LunarisDolby.git packages/apps/LunarisDolby
