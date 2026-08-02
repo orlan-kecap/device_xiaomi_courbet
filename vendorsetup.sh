d2341b654533b236864c19c4f863523cd130175b
一スAciss <orlannihboss@gmail.com>
2026-08-03 01:04:22 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -6,7 +6,7 @@ git clone https://github.com/orlan-kecap/vendor_xiaomi_sm6150-common.git -b 16.2
 git clone https://github.com/orlan-kecap/vendor_xiaomi_courbet.git -b 16.2 vendor/xiaomi/courbet
 
 # kernel
-git clone https://github.com/orlan-kecap/Aciss-mainline.git -b los kernel/xiaomi/sm6150
+git clone https://github.com/orlan-kecap/Aciss-mainline.git -b los kernel/xiaomi/sm6150 --depth 1
 
 # hardware 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
