a9330bfb67ad28ec16ec19cc667519d8cd0b21f8
Teh <orlannihboss@gmail.com>
2026-08-02 14:17:32 +0700

    Update vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -7,11 +7,6 @@ git clone https://github.com/orlan-kecap/vendor_xiaomi_sm6150-common.git -b 16.2
 git clone https://github.com/orlan-kecap/vendor_xiaomi_courbet.git -b 16.2 vendor/xiaomi/courbet
 
 # kernel
-git clone https://github.com/orlan-kecap/Daendless-Road.git -b A16 kernel/xiaomi/sm6150
-
-# miui camera
-git clone https://github.com/orlan-kecap/vendor_xiaomi_miuicamera-sweet.git -b 16.2 vendor/xiaomi/miuicamera-sweet
-git clone https://github.com/orlan-kecap/device_xiaomi_miuicamera-sweet.git -b 16.2 device/xiaomi/miuicamera-sweet
-
+git clone https://github.com/orlan-kecap/Aciss-mainline.git -b los kernel/xiaomi/sm6150
 # hardware 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-23.2 hardware/xiaomi
